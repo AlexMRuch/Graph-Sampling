e76773baf339a2b86a3faf5425c978bad7f8adff
Alex Ruch <amr442@cornell.edu>
2018-11-10 21:26:55 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,12 +1,14 @@
 import sampling 
 import networkx as nx
 from graph_tool.all import *
-from time import time
+import timeit  #run with timeit.timer().timeit(number=5)
 
 # make random graph: graph-tool.skewed.de/static/doc/generation.html#graph_tool.generation.price_network
+print('Making random graph for graph-tool and networkx...')
 g_gt = graph_tool.generation.price_network(100000, directed=False)
 g_gt.save("test.graphml")
 g_nx = nx.read_edgelist("test.graphml", create_using= nx.Graph(),nodetype=int)
+print('  Graph made and loaded into both libraries')
 
 # make an object and call function SRW
 object1 = sampling.SRW_RWF_ISRW()
@@ -33,19 +35,19 @@ print("Number of nodes sampled=",len(sample3.nodes()))
 print("Number of edges sampled=",len(sample3.edges()))
 
 # make an object and call function FF
-object4=sampling.ForestFire()
+object4 = sampling.ForestFire()
 sample4 = object4.forestfire(g,28000) # graph, number of nodes to sample
 print("Number of nodes sampled=",len(sample4.nodes()))
 print("Number of edges sampled=",len(sample4.edges()))
 
 # make an object and call function MHRW
-object5=sampling.MHRW()
+object5 = sampling.MHRW()
 sample5 = object5.mhrw(g,28000,30) # graph, number of nodes to sample, node
 print("Number of nodes sampled=",len(sample5.nodes()))
 print("Number of edges sampled=",len(sample5.edges()))
 
 # make an object and call function TIES
-object6=sampling.TIES()
+object6 = sampling.TIES()
 sample6 = object6.ties(g,10,0.01) # graph, number of nodes to sample, phi
 print("Number of nodes sampled=",len(sample6.nodes()))
 print("Number of edges sampled=",len(sample6.edges()))
